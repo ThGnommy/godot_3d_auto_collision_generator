d6fee6bc0e02fc75d8db941beaf6ca584f1586d7
thgnommy <th.brandoli@gmail.com>
2024-02-11 22:46:15 +0100

    added MultipleConvexCollisionsSettings scene and script

---
+++ addons/auto_collision_creator/multiple_convex_collisions_settings.gd
@@ -0,0 +1,12 @@
+extends Control
+
+# docs: https://docs.godotengine.org/en/4.1/classes/class_meshconvexdecompositionsettings.html
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
